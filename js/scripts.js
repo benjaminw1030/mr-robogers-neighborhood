e16467a4d7466e0e0347a65f7aaa7aabd008905b
Ben Wilson <benjaminw1030@gmail.com>
2021-08-20 09:09:32 -0700

    pass output 0 spec

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// "0" output "0" //
+// "1" output "Beep!"
+// "2" output "Boop!"
+// "3" output "Won't you be my neighbor?"
+// "14" output "Beep!"
+// "12" output "Boop!"
+// "123" output "Won't you be my neighbor?"
+// "10" output "0, Beep!, Boop!, Won't you be my neighbor?, 4, 5, 6, 7, 8, 9 Beep!"
+//Business Logic
+
+function mrRobogers(number) {
+  return number
+}
+
+mrRobogers("0");
